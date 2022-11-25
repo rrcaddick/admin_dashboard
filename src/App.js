bad6bbd957f63d0e8dd9f1486b08368aa190597b
Ray Caddick <rrcaddick@gmail.com>
2022-11-25 08:46:00 +0200

    Project setup. Theme, routes, topbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContex, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import TopBar from "./scenes/global/top-bar";
+import Sidebar from "./scenes/global/sidebar";
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Contacts from "./scenes/contacts";
+// import Invoices from "./scenes/invoices";
+// import Form from "./scenes/form";
+// import Bar from "./scenes/bar";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContex.Provider value={colorMode}>
+      <ProSidebarProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <Sidebar />
+            <main className="content">
+              <TopBar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                {/* <Route path="/team" element={<Team />} /> */}
+                {/* <Route path="/contacts" element={<Contacts />} /> */}
+                {/* <Route path="/invoices" element={<Invoices />} /> */}
+                {/* <Route path="/form" element={<Form />} /> */}
+                {/* <Route path="/bar" element={<Bar />} /> */}
+                {/* <Route path="/line" element={<Line />} /> */}
+                {/* <Route path="/pie" element={<Pie />} /> */}
+                {/* <Route path="/faq" element={<FAQ />} /> */}
+                {/* <Route path="/geography" element={<Geography />} /> */}
+                {/* <Route path="/calendar" element={<Calendar />} /> */}
+              </Routes>
+            </main>
+          </div>
+        </ThemeProvider>
+      </ProSidebarProvider>
+    </ColorModeContex.Provider>
   );
 }
 
---
+++ src/scenes/global/top-bar.jsx
@@ -0,0 +1,44 @@
+import { useContext } from "react";
+import { Box, IconButton, useTheme, InputBase } from "@mui/material";
+import { ColorModeContex, tokens } from "../../theme";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const TopBar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContex);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* Search Bar */}
+      <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+      {/* Icons */}
+      <Box display="flex">
+        <IconButton type="button" sx={{ p: 1 }} onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? <LightModeOutlinedIcon /> : <DarkModeOutlinedIcon />}
+        </IconButton>
+        <IconButton type="button" sx={{ p: 1 }}>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton type="button" sx={{ p: 1 }}>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default TopBar;
